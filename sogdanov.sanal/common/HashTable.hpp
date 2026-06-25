b944ef0c1ea1cbf6029a0ea08109ae67bd75ec1d
kuz <sanalsogdanov111@gmail.com>
2026-06-25 11:50:47 +0300

    move template implementations to header

--- sogdanov.sanal/common/HashTable.hpp
+++ sogdanov.sanal/common/HashTable.hpp
@@ -15,14 +15,71 @@ namespace sogdanov {
   };
 
   template< class Key, class Value >
-  void initHashTable(HashTable< Key, Value >& table, const size_t initialCapacity = 16);
+  void initHashTable(HashTable< Key, Value >& table, const size_t initialCapacity = 16) {
+    table.capacity = initialCapacity;
+    table.size = 0;
+    table.keys = new Key[initialCapacity];
+    table.values = new Value[initialCapacity];
+    table.occupied = new bool[initialCapacity]{false};
+  }
 
   template< class Key, class Value >
-  void destroyHashTable(HashTable< Key, Value >& table);
+  void destroyHashTable(HashTable< Key, Value >& table) {
+    delete[] table.keys;
+    delete[] table.values;
+    delete[] table.occupied;
+    table.keys = nullptr;
+    table.values = nullptr;
+    table.occupied = nullptr;
+    table.capacity = 0;
+    table.size = 0;
+  }
 
   template< class Key, class Value >
-  bool insertHash(HashTable< Key, Value >& table, const Key& key, const Value& value);
+  bool insertHash(HashTable< Key, Value >& table, const Key& key, const Value& value) {
+    if (table.size * 2 >= table.capacity) {
+      const size_t oldCapacity = table.capacity;
+      Key* const oldKeys = table.keys;
+      Value* const oldValues = table.values;
+      bool* const oldOccupied = table.occupied;
+
+      table.capacity *= 2;
+      table.keys = new Key[table.capacity];
+      table.values = new Value[table.capacity];
+      table.occupied = new bool[table.capacity]{false};
+      table.size = 0;
+
+      for (size_t i = 0; i < oldCapacity; ++i) {
+        if (oldOccupied[i]) {
+          insertHash(table, oldKeys[i], oldValues[i]);
+        }
+      }
+      delete[] oldKeys;
+      delete[] oldValues;
+      delete[] oldOccupied;
+    }
+
+    const size_t h1 = key % table.capacity;
+    const size_t h2 = (key % (table.capacity - 1)) | 1;
+    size_t index = h1;
+
+    for (size_t i = 0; i < table.capacity; ++i) {
+      if (!table.occupied[index]) {
+        table.keys[index] = key;
+        table.values[index] = value;
+        table.occupied[index] = true;
+        table.size++;
+        return true;
+      }
+      if (table.keys[index] == key) {
+        return false;
+      }
+      index = (index + h2) % table.capacity;
+    }
+    return false;
+  }
 
 }
 
 #endif
+
